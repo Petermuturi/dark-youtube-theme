39fd98a7dd4670272e93abb32d4eaefad991a548
nicu03 <nicolae.natrapeiu@gmail.com>
2015-03-22 22:50:06 +0200

    greyed out the comments section! other minor fixes by just enforcing rules with important

--- chrome/trafficRedirect.js
+++ chrome/trafficRedirect.js
@@ -1,10 +1,17 @@
 chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        // console.log("This is a first install!");
-        window.open('http://nicu03.github.io/?new-install=chrome','_blank');
-    }else if(details.reason == "update"){
-        var thisVersion = chrome.runtime.getManifest().version;
-        window.open('http://nicu03.github.io/?install=chrome&update-from='+details.previousVersion+'&to='+ thisVersion, '_blank');
-        // console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-    }
+    window.open('http://nicu03.github.io/?platform=chrome&v=' +
+                 chrome.runtime.getManifest().version +
+                 (details.reason == "update" ? "&p=" + details.previousVersion +
+                  "&type=upgrade" : "&type=install"),
+                 '_blank');
+
+    // if(details.reason == "install"){
+    //     window.open('http://nicu03.github.io/?platform=chrome
+    //         &type=install', '_blank');
+    // }else if(details.reason == "update"){
+    //     var thisVersion = chrome.runtime.getManifest().version;
+    //     window.open('http://nicu03.github.io/?platform=chrome
+    //         &v='+ details.previousVersion + '&p=' + thisVersion +
+    //         '&type=upgrade"', '_blank');
+    // }
 });

8d3bc4e1874dd6bac712c1df47211a3978b3121b
nicu03 <nicolae.natrapeiu@gmail.com>
2015-08-31 22:30:34 +0300

    basic toggle

--- ffox/lib/main.js
+++ ffox/lib/main.js
@@ -1,16 +1,34 @@
 var pageMod = require("sdk/page-mod");
 var self = require("sdk/self");
-pageMod.PageMod({
-  include: ["https://www.youtube.*", "http://www.youtube.*"],
-  contentStyleFile: self.data.url("dark.css")
-});
-pageMod.PageMod({
-    include: [
-    /.*:\/\/apis\.google\.com\/.*\/widget\/render\/comments.*/,
-    /.*:\/\/plus\.googleapis\.com\/.*\/widget\/render\/comments.*/
-    ],
-    contentStyleFile: self.data.url("dark_comments.css")
-});
+
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var switchButton = ToggleButton({
+    id: "switch-button",
+    label: "Toggle DarkYoutube Theme",
+    icon: {
+      "16": self.data.url("icon16.png"),
+      "32": self.data.url("icon32.png"),
+      "64": self.data.url("icon64.png")
+    },
+    onChange: function(state) {
+      if(state.checked == true){
+        pageMod.PageMod({
+          include: ["https://www.youtube.*", "http://www.youtube.*"],
+          contentStyleFile: self.data.url("dark.css")
+        });
+        pageMod.PageMod({
+            include: [
+            /.*:\/\/apis\.google\.com\/.*\/widget\/render\/comments.*/,
+            /.*:\/\/plus\.googleapis\.com\/.*\/widget\/render\/comments.*/
+            ],
+            contentStyleFile: self.data.url("dark_comments.css")
+        });
+      }
+    }
+  });
+switchButton.click(); // turn on the theme
+
+
 // --------------------------------WELCOME PAGE---------------------------------
 (function () {
   var prefs = require("sdk/simple-prefs").prefs;

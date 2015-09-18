aa15e44c396ef95fee51bcc876f150065ff118d1
nicu03 <nicolae.natrapeiu@3pillarglobal.com>
2015-09-18 15:24:48 +0300

    fixed chrome issue, fixed warning

--- chrome/trafficRedirect.js
+++ chrome/trafficRedirect.js
@@ -1,24 +1,16 @@
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
-        window.open('http://nicu03.github.io/?platform=chrome&v=' + 
-        chrome.runtime.getManifest().version + 
+        window.open('http://nicu03.github.io/?platform=chrome&v=' +
+        chrome.runtime.getManifest().version +
         "&type=install",
         '_blank')
     }else if(details.reason == "update"){
-        window.open('http://nicu03.github.io/?platform=chrome&v=' + 
-        chrome.runtime.getManifest().version +
-        '&p=' + details.previousVersion +
-        '&type=upgrade"',
-        '_blank');
+        if (chrome.runtime.getManifest().version != details.previousVersion) {
+            window.open('http://nicu03.github.io/?platform=chrome&v=' +
+            chrome.runtime.getManifest().version +
+            '&p=' + details.previousVersion +
+            '&type=upgrade',
+            '_blank');
+        }
     }
-    
-    // if(details.reason == "install"){
-    //     window.open('http://nicu03.github.io/?platform=chrome
-    //         &type=install', '_blank');
-    // }else if(details.reason == "update"){
-    //     var thisVersion = chrome.runtime.getManifest().version;
-    //     window.open('http://nicu03.github.io/?platform=chrome
-    //         &v='+ details.previousVersion + '&p=' + thisVersion +
-    //         '&type=upgrade"', '_blank');
-    // }
 });

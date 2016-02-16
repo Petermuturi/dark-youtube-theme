e41a2b0c26d7d91566e7be4444171686539a5b89
nicu03 <nicolae.natrapeiu@3pillarglobal.com>
2016-02-17 00:23:38 +0200

    first opera build; right to 2.0

--- opera/trafficRedirect.js
+++ opera/trafficRedirect.js
@@ -1,12 +1,12 @@
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
-        window.open('http://nicu03.github.io/?platform=chrome&v=' +
+        window.open('http://nicu03.github.io/?platform=opera&v=' +
         chrome.runtime.getManifest().version +
         "&type=install",
         '_blank')
     }else if(details.reason == "update"){
         if (chrome.runtime.getManifest().version != details.previousVersion) {
-            window.open('http://nicu03.github.io/?platform=chrome&v=' +
+            window.open('http://nicu03.github.io/?platform=opera&v=' +
             chrome.runtime.getManifest().version +
             '&p=' + details.previousVersion +
             '&type=upgrade',
